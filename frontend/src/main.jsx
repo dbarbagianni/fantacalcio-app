cf477c134ce31c8fca4aa65b64c68ca8bbb7eef1
dbarbagianni <mimmo_gt4@libero.it>
2025-02-24 17:23:37 +0100

    Aggiunti file mancanti

---
+++ frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return <h1>Ciao Domenico, il frontend è pronto!</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
